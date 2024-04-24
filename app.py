676cdafb9671d9caa58258d4025f045e1d2577d0
Tushar Talukder <tushartalukder9797@gmail.com>
2024-04-24 15:34:02 +0600

    Update app.py

--- app.py
+++ app.py
@@ -221,9 +221,9 @@ def apply_mask(image, mask, color=(255, 0, 0), alpha=0.5):
     masked_image = image * (1 - alpha * mask) + alpha * mask * color
     
     # Clip values to be in the range [0, 255]
-    masked_image = masked_image/255.0
+    # masked_image = masked_image/255.0
     
-    # masked_image = tf.clip_by_value(masked_image, 0, 255)
+    masked_image = tf.clip_by_value(masked_image, 0, 1)
     
     # Convert the masked image to uint8
     # masked_image = tf.cast(masked_image, tf.uint8)
